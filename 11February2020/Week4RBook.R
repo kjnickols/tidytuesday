1296095bf6d7c6e249dcf75523d849d995216903
kjnickols <kerry.nickols@csun.edu>
2020-02-25 17:25:55 -0800

    I added to the last thing

--- 11February2020/Week4RBook.R
+++ 11February2020/Week4RBook.R
@@ -78,3 +78,92 @@ diamonds %>%
 #2 What does geom_col() do? I GIVE UP!!!!!!!!!
 ggplot(data = diamonds) +
   geom_col(mapping = aes(y = cut))
+#The above didn't work
+
+#Adding on starting 2/25/20
+ggplot() + geom_col(data = diamonds, aes(x = cut, y = depth))
+#geom_col does not histogram - it does stuff based on stats/summary, like a normal 
+#bar chart
+
+#3 - geoms and stats come in pairs, what do they have in common? (I got lazy and 
+#didn't do this)
+# stat_bin(geom="bar") does the same as geom_bar(stat="bin") 
+
+#4 What variables does stat_smooth() compute? What parameters control its behaviour?
+#smooths data using different methods. needs a formula and a method
+
+#5 In our proportion bar chart, we need to set group = 1. Why? In other words what 
+#is the problem with these two graphs?
+
+#graph 1
+ggplot(data = diamonds) + 
+  geom_bar(mapping = aes(x = cut, y = ..prop..))
+ggplot(data = diamonds) + 
+  geom_bar(mapping = aes(x = cut, y = ..prop.., group = 1))
+#need group to show that they are all part of the same group
+
+#graph 2
+ggplot(data = diamonds) + 
+  geom_bar(mapping = aes(x = cut, fill = color, y = ..prop..))
+ggplot(data = diamonds) + 
+  geom_bar(mapping = aes(x = cut,  y = ..prop.., group = 1, fill = color))
+#I think for both of these they were showing each as a group, but I couldn't figure 
+#what to do about the second one with color.
+
+#you can color a bar chart with color or with fill
+ggplot(data = diamonds) + 
+  geom_bar(mapping = aes(x = cut, colour = cut))
+#the one above does an outline
+
+ggplot(data = diamonds) + 
+  geom_bar(mapping = aes(x = cut, fill = cut)) #this one is solid fill
+#add another dimension by setting fill to another thing
+ggplot(data = diamonds) + 
+  geom_bar(mapping = aes(x = cut, fill = clarity))
+
+#if you don't want stacked bars:
+ggplot(data = diamonds, mapping = aes(x = cut, fill = clarity)) + 
+  geom_bar(alpha = 1/5, position = "identity") #here they overlap w/transparency 
+# (weird)
+ggplot(data = diamonds, mapping = aes(x = cut, colour = clarity)) + 
+  geom_bar(fill = NA, position = "identity") #here they overlap with just outlines
+
+#if want to compare proportions and not values:
+ggplot(data = diamonds) + 
+  geom_bar(mapping = aes(x = cut, fill = clarity), position = "fill")
+
+#if you want to compare values:
+ggplot(data = diamonds) + 
+  geom_bar(mapping = aes(x = cut, fill = clarity), position = "dodge")
+
+#R won't plot all the points if they overlap - there's a limit, so if you want to see
+#all the data points you can add some random noise using jitter
+ggplot(data = mpg) + 
+  geom_point(mapping = aes(x = displ, y = hwy), position = "jitter")
+
+#Exercise 1: What is the problem with this plot? How could you improve it?
+ggplot(data = mpg, mapping = aes(x = cty, y = hwy)) + 
+  geom_point()
+#overlapping points
+ggplot(data = mpg) +
+  geom_point(mapping = aes(x = cty, y = hwy), position = "jitter") 
+
+#Exercise 2: What parameters to geom_jitter() control the amount of jittering?
+#width and height
+
+#Exercise 3: Compare and contrast geom_jitter() with geom_count()
+#Maps to area corresponding with number of points
+ggplot(data = mpg) +
+  geom_jitter(mapping = aes(x = cty, y = hwy))
+ggplot(data = mpg) +
+  geom_count(mapping = aes(x = cty, y = hwy))
+#I like geom_count WAY better
+
+#Exercise 4: What’s the default position adjustment for geom_boxplot()? Create a 
+#visualisation of the mpg dataset that demonstrates it.
+#dodge2
+ggplot(mpg, aes(x = class, y = hwy)) +
+  geom_boxplot()
+
+ggplot(mpg, aes(x = class, y = hwy)) +
+  geom_boxplot(aes(color=drv))
